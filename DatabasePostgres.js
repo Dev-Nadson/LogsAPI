173916c1cd9025e5dcf055f0a5da1ed2b9ab86d1
Dev Nadson <nadson.pros@gmail.com>
2025-08-26 23:13:00 -0300

    Feat: Função create realiza inserções batch de três em três

--- DatabasePostgres.js
+++ DatabasePostgres.js
@@ -2,11 +2,20 @@ import sql from './db.js'
 
 export class Database {
     async create(logs) {
-        const { pathname, method, status_code, duration, started_at } = logs
+        if (!Array.isArray(logs) || logs.length === 0) {
+            throw new Error('Dados inválidos, não é um array')
+        }
 
-        await sql`insert into logs (pathname, method, status_code, duration, started_at) VALUES (${pathname}, ${method}, ${status_code}, ${duration}, ${started_at})`
+        await Promise.all(
+            logs.map(async log => {
+                const { pathname, method, status_code, duration, started_at } = log
+                return sql`
+                INSERT INTO logs (pathname, method, status_code, duration, started_at)
+                VALUES (${pathname}, ${method}, ${status_code}, ${duration}, ${started_at})
+                `
+            })
+        )
     }
-
     async list() {
         const data = await sql`select * from logs`
         return data
